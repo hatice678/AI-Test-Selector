dab4db22bd9ef90e36438601e86c8db77f80d449
hatice678 <haticesimsekehm2@gmail.com>
2025-09-18 15:58:56 +0300

    secon

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,7 +2,7 @@ import pytest
 from src.user import create_user
 
 def test_create_user_valid():
-    user = create_user("hatice")
+    user = create_user("hatice2")
     assert user["active"] == True
 
 def test_create_user_valid():
