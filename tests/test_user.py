def3e7b9b5730c464566b022bdfa003e13d824fa
hatice678 <haticesimsekehm2@gmail.com>
2025-09-19 14:49:55 +0300

    modelData6

--- tests/test_user.py
+++ tests/test_user.py
@@ -13,14 +13,5 @@ def test_create_user_invalid():
     with pytest.raises(ValueError):
         create_user("")
 
-def test_force_fail():
-    assert False
 
-def test_create_user_invalid():
-    with pytest.raises(ValueError):
-        create_user("")  # ✅ geçmeli
 
-def test_create_user_fail_on_number():
-    # ❌ kasıtlı hata: sayıyı da kabul etmeye çalışalım
-    user = create_user(123)
-    assert user["active"] == True
